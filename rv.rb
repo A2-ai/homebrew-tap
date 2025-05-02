4a689c4bb7b24a5aad8051ee56150e8a31d2c3d1
Devin Pastoor <devin.pastoor@gmail.com>
2025-05-02 19:55:57 -0400

    fake

--- rv.rb
+++ rv.rb
@@ -1,1 +1,24 @@
-version "0.1"
+class Rv < Formula
+    desc "rv - a fast R package installer"
+    homepage "https://github.com/a2-ai/rv"
+    version "0.1"
+  
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/a2-ai/rv/releases/download/v0.4.1/rv-v0.4.1-x86_64-apple-darwin.tar.gz[arm64]"
+        sha256 "97ddb493c94ae6be7f0db1ed4e9e496ac5275a2fa854176b905e808f4fcfbcbd[arm64]"
+      elsif Hardware::CPU.intel?
+        url "https://github.com/a2-ai/rv/releases/download/v0.4.1/rv-v0.4.1-x86_64-apple-darwin.tar.gz[x64]"
+        sha256 "97ddb493c94ae6be7f0db1ed4e9e496ac5275a2fa854176b905e808f4fcfbcbd[x64]"
+      end
+    end
+  
+    def install
+      bin.install "rv"
+    end
+  
+    test do
+      system "#{bin}/rv", "--version"
+    end
+  end
+  
