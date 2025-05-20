dea12bb63a9e53541c2df3f947b620941b425c3c
Devin Pastoor <devin.pastoor@gmail.com>
2025-05-20 13:07:31 -0400

    rustwrap update: rv.rb

--- rv.rb
+++ rv.rb
@@ -1,15 +1,15 @@
 class Rv < Formula
   desc "rv - a fast R package installer"
   homepage "https://github.com/a2-ai/rv"
-  version "0.6.1"
+  version "0.6.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/a2-ai/rv/releases/download/v0.6.1/rv-v0.6.1-aarch64-apple-darwin.tar.gz"
-      sha256 "03e94fa9deb0baaf1b663da44dbb6a74d9ba5e488406df66289362fc285adb4a"
+      url "https://github.com/a2-ai/rv/releases/download/v0.6.2/rv-v0.6.2-aarch64-apple-darwin.tar.gz"
+      sha256 "500b7e7f582acec17e6b6229e29d19b605ea622e0fa8ac4ac6118eec3b82965a"
     elsif Hardware::CPU.intel?
-      url "https://github.com/a2-ai/rv/releases/download/v0.6.1/rv-v0.6.1-x86_64-apple-darwin.tar.gz"
-      sha256 "3d72e730b3e977687bd1b0f9161b6f69f56cd37240e926ba1c3e43f13cde4e30"
+      url "https://github.com/a2-ai/rv/releases/download/v0.6.2/rv-v0.6.2-x86_64-apple-darwin.tar.gz"
+      sha256 "c99b7fbe5cc3bb0c6d7404888ae3f77d13f67ddb7839bf1f58803b4e6e461761"
     end
   end
 
