efd08318aa6303eeebe83db70565320f91f8ae36
Devin Pastoor <devin.pastoor@gmail.com>
2025-09-10 21:36:35 -0400

    rustwrap update: link-checker.rb

--- link-checker.rb
+++ link-checker.rb
@@ -1,15 +1,15 @@
 class LinkChecker < Formula
   desc "link-checker - a fast and simple link checker"
   homepage "https://github.com/a2-ai/link-checker"
-  version "0.2.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/a2-ai/link-checker/releases/download/v0.2.0/link-checker-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "e0e3fff89d7b56400a2cb45171f6c6cbc92a8a53d6270fc6a21ede836a9b8d89"
+      url "https://github.com/a2-ai/link-checker/releases/download/v0.4.0/link-checker-v0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "d1e8d84ab714e99d1ccd9cc66fb178cf813dfd7fbfd5e1c8c2dadb11e10c6395"
     elsif Hardware::CPU.intel?
-      url "https://github.com/a2-ai/link-checker/releases/download/v0.2.0/link-checker-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "8857b37622b3767aa80ec9df097f1c57db44056bfdcaf362adcdd0470424dd50"
+      url "https://github.com/a2-ai/link-checker/releases/download/v0.4.0/link-checker-v0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "724597b35a329306e78f55867b1ecae19f6153c198aa8ff22545b7b617df58f0"
     end
   end
 
